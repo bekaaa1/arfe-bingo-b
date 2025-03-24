e5fa18a8725d67c5a2f6653fc3efc0a024560c83
bekaaa1 <bgirma057@gmail.com>
2025-03-24 08:03:16 +0300

    Update sw.js

--- Arife bingo_web_project_pwa_admin/sw.js
+++ Arife bingo_web_project_pwa_admin/sw.js
@@ -1,21 +1,24 @@
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('bingo-cache').then((cache) => {
-            return cache.addAll([
-                '/index.html',
-                '/login.html',
-                '/styles.css',
-                '/script.js',
-                '/auth.js'
-            ]);
-        })
+const CACHE_NAME = 'bingo-casino-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/app.js',
+    '/manifest.json',
+    '/assets/icons/icon-192.png',
+    '/assets/icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
     );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request)
+            .then(response => response || fetch(e.request))
     );
 });
